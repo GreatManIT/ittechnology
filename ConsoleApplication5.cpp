c72e09e2a5ca7d851247164d3f8781f192d29780
GreatManIT <belckovsasa@gmail.com>
2021-10-31 17:08:43 +0200

    genuine

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "*";
+	}
+	for (int i = 0; i < 10; i += 10)
+	{
+		cout << "\t*"<<endl;
+		for (int i = 0; i < 10; i += 10)
+		{
+			cout << "*";
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "*";
+	}
+
+	cout << "\n";
+	system("pause");
+}
+
+
