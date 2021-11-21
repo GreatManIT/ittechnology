4527a986d5277fee489066d022385daa5bd59774
GreatManIT <belckovsasa@gmail.com>
2021-11-21 21:25:25 +0200

    +file

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "*";
+	}
+	for (int i = 0; i < 10; i += 10)
+	{
+		cout << "\t Good*"<<endl;
+		for (int i = 0; i < 10; i += 10)
+		{
+			cout << "*";
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "*";
+	}
+	cout << "Hello World!" << endl;
+	cout << "I`m ready!" << endl;
+
+
+	cout << "\n";
+	system("pause");
+}
+
+
